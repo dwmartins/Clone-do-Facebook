74404ce3f88390115184c8e370932f8187aec865
Douglas Wellington Martins <90710466+dwmartins@users.noreply.github.com>
2021-12-23 14:26:46 -0300

    acrescentado o script

--- Js/script.js
+++ Js/script.js
@@ -17,3 +17,18 @@ password.addEventListener('blur',()=>{
     password.style.borderColor = "#ccc"
 })
 
+
+// login 
+
+function entrar(){
+    
+    var login = document.getElementById('email').value;
+    var senha = document.getElementById('password').value;
+
+    if(login == '' || senha == ''){
+        alert('complete os campos por favor');
+    }else {
+        document.getElementById('formulario').submit();
+    }
+    
+}
